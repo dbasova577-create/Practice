f239336d0bca07576277ba133d2c2d8ebb60f42c
Basova Daria <dbasova577@gmail.com>
2026-07-13 23:43:39 +0300

    fix: resolve review, add docs and fix file paths

--- test_calculator.py
+++ test_calculator.py
@@ -30,14 +30,24 @@ def test_night_error(invalid_nights):
 
 
 @pytest.mark.parametrize('invalid_adults',[0,-1])
-
 def test_adults_error(invalid_adults):
     with pytest.raises(ValueError, match='должен быть хотя бы один взрослый'):
         calculator_hotel(4,3000,invalid_adults,0,'AI',0)
 
 
 @pytest.mark.parametrize('invalid_discount',[-0.1,1.1,2.0])
-
 def test_discount(invalid_discount):
     with pytest.raises(ValueError, match='скидка должна быть от 0 до 1'):
         calculator_hotel(7, 4000,3,0,'AI',invalid_discount)
+
+
+@pytest.mark.parametrize('invalid_price', [-1, -5, -10])
+def test_price_error(invalid_price):
+    with pytest.raises(ValueError, match='стоимость ночи не может быть отрицательной'):
+        calculator_hotel(nights=7, price_per_night_per_person=invalid_price, adults=2, children=0, board='AI', discount=0)
+
+
+@pytest.mark.parametrize('invalid_children', [-1, -5, -10])
+def test_children_error(invalid_children):
+    with pytest.raises(ValueError, match='количество детей не может быть отрицательным'):
+        calculator_hotel(nights=7, price_per_night_per_person=5000, adults=2, children=invalid_children, board='AI', discount=0)
